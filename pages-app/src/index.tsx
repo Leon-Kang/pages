9bbf02227e56df80d373a70c5ddfa171548f7af2
leon <coderleonkang@gmail.com>
2022-12-17 22:34:02 +0800

    style update

--- pages-app/src/index.tsx
+++ pages-app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import MainNav from './MainNav'
+import MainNav from './components/MainNav'
 import reportWebVitals from './reportWebVitals';
 
 
---
+++ pages-app/src/pages/Home/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./styles.css";
+class Home extends React.Component<{}> {
+    render() {
+        return (
+            <div className="home">
+                <div className="left">
+                    111111
+                </div>
+                <div className="right">
+                    2222
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
